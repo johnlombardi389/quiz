dae677d725c629a40a911c6a742571866227b4d3
johnlombardi389 <lombardij38@gmail.com>
2023-01-30 19:54:41 -0500

    Set up ternary to display get username or quiz based and added props to home

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,9 @@
+const Quiz = () => {
+  return (
+    <div>
+      <h1>Quiz Questions</h1>
+    </div>
+  );
+};
+
+export default Quiz;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 // Components
 import GetUsername from "../components/GetUsername";
+import Quiz from "../components/Quiz";
 // Style
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
-const Home = () => {
+const Home = ({ username, setUsername, score, setScore, previousScore }) => {
   const [quizStatus, setQuizStatus] = useState(false);
 
   return (
     <StyledHome>
-      <GetUsername />
+      {quizStatus ? (
+        <Quiz />
+      ) : (
+        <GetUsername quizStatus={quizStatus} setQuizStatus={setQuizStatus} />
+      )}
     </StyledHome>
   );
 };
 
 const StyledHome = styled(motion.div)`
+  background: pink;
   max-width: 1900px;
   margin: 0 auto;
 `;
