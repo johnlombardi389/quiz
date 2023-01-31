ad272b657966148842cd32ecc128761dd466d45c
johnlombardi389 <lombardij38@gmail.com>
2023-01-30 19:32:57 -0500

    Added questions

--- src/questions.jsx
+++ src/questions.jsx
@@ -1,21 +1,220 @@
 export const questions = [
+  // Question 1
   {
-    ask: "Everything in React is a _____",
+    ask: "What is the name of the largest ocean in the world?",
     options: [
       {
-        answer: "Package",
+        answer: "Arctic",
         isCorrect: false,
       },
       {
-        answer: "Component",
+        answer: "Pacific",
         isCorrect: true,
       },
       {
-        answer: "Class",
+        answer: "Atlantic",
         isCorrect: false,
       },
       {
-        answer: "Prop",
+        answer: "Indian",
+        isCorrect: false,
+      },
+    ],
+  },
+  // Question 2
+  {
+    ask: "How many countries are there in the United Kingdom?",
+    options: [
+      {
+        answer: "2",
+        isCorrect: false,
+      },
+      {
+        answer: "3",
+        isCorrect: false,
+      },
+      {
+        answer: "4",
+        isCorrect: true,
+      },
+      {
+        answer: "5",
+        isCorrect: false,
+      },
+    ],
+  },
+  // Question 3
+  {
+    ask: "Where are the Spanish Steps located?",
+    options: [
+      {
+        answer: "Guadalajara, Mexico",
+        isCorrect: false,
+      },
+      {
+        answer: "Barcelona, Spain",
+        isCorrect: false,
+      },
+      {
+        answer: "Buenos Aires, Argentina",
+        isCorrect: false,
+      },
+      {
+        answer: "Rome, Italy",
+        isCorrect: true,
+      },
+    ],
+  },
+  // Question 4
+  {
+    ask: "Which U.S. state is known as The Garden State?",
+    options: [
+      {
+        answer: "New Jersey",
+        isCorrect: true,
+      },
+      {
+        answer: "Florida",
+        isCorrect: false,
+      },
+      {
+        answer: "Iowa",
+        isCorrect: false,
+      },
+      {
+        answer: "Texas",
+        isCorrect: false,
+      },
+    ],
+  },
+  // Question 5
+  {
+    ask: "What is the capital city of South Korea?",
+    options: [
+      {
+        answer: "Busan",
+        isCorrect: false,
+      },
+      {
+        answer: "Incheon",
+        isCorrect: false,
+      },
+      {
+        answer: "Seoul",
+        isCorrect: true,
+      },
+      {
+        answer: "Daejeon",
+        isCorrect: false,
+      },
+    ],
+  },
+  // Question 6
+  {
+    ask: "By area, what is the smallest country in the world?",
+    options: [
+      {
+        answer: "Gibraltar",
+        isCorrect: false,
+      },
+      {
+        answer: "San Marino",
+        isCorrect: false,
+      },
+      {
+        answer: "Liechtenstein",
+        isCorrect: false,
+      },
+      {
+        answer: "Vatican City",
+        isCorrect: true,
+      },
+    ],
+  },
+  // Question 7
+  {
+    ask: "Which country has the longest coastline in the world?",
+    options: [
+      {
+        answer: "Australia",
+        isCorrect: false,
+      },
+      {
+        answer: "Brazil",
+        isCorrect: false,
+      },
+      {
+        answer: "Canada",
+        isCorrect: true,
+      },
+      {
+        answer: "Africa",
+        isCorrect: false,
+      },
+    ],
+  },
+  // Question 8
+  {
+    ask: "Which country is Mt. Fuji located in?",
+    options: [
+      {
+        answer: "Nepal",
+        isCorrect: false,
+      },
+      {
+        answer: "Indonesia",
+        isCorrect: false,
+      },
+      {
+        answer: "Cambodia",
+        isCorrect: false,
+      },
+      {
+        answer: "Japan",
+        isCorrect: true,
+      },
+    ],
+  },
+  // Question 9
+  {
+    ask: "What is the name of the world’s largest island?",
+    options: [
+      {
+        answer: "Greenland",
+        isCorrect: true,
+      },
+      {
+        answer: "New Guinea",
+        isCorrect: false,
+      },
+      {
+        answer: "Borneo",
+        isCorrect: false,
+      },
+      {
+        answer: "Madagascar",
+        isCorrect: false,
+      },
+    ],
+  },
+  // Question 10
+  {
+    ask: "How many time zones does Russia have?",
+    options: [
+      {
+        answer: "3",
+        isCorrect: false,
+      },
+      {
+        answer: "7",
+        isCorrect: false,
+      },
+      {
+        answer: "11",
+        isCorrect: true,
+      },
+      {
+        answer: "14",
         isCorrect: false,
       },
     ],
