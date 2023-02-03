a5eef43b29de47ea5b76d9a9d7a7e3421d60cac6
johnlombardi389 <lombardij38@gmail.com>
2023-02-02 21:54:05 -0500

    Created score slice file and added initial state for score

---
+++ src/features/score/scoreSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  score: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "score",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.score += 1;
+    },
+    reset: (state) => {
+      state.score = 0;
+    },
+  },
+});
