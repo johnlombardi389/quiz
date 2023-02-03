733a12ec7977cab628d6dcc85159fec19004937e
johnlombardi389 <lombardij38@gmail.com>
2023-02-02 22:19:52 -0500

    Added score reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import scoreReducer from "../features/score/scoreSlice";
 
 export const store = configureStore({
   reducer: {
-    //all reducers we make will go in here
+    score: scoreReducer,
   },
 });
